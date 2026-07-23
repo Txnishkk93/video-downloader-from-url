cc276960ab6593e5f65f7340b79dc97c137a80d2
Txnishkk93 <rajputt4ni5hk@gmail.com>
2026-07-23 15:19:41 +0530

    fix: next.js config schema for outputFileTracingIncludes

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    outputFileTracingIncludes: {
-      '/api/**/*': ['./node_modules/youtube-dl-exec/bin/**/*'],
-    },
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./node_modules/youtube-dl-exec/bin/**/*'],
   },
 };
 
