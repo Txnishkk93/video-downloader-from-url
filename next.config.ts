563ead7f9bf21cd9e1efe0e8bb781a56831e0bc0
Txnishkk93 <rajputt4ni5hk@gmail.com>
2026-07-23 15:18:17 +0530

    fix: bundle yt-dlp binary explicitly in vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/**/*': ['./node_modules/youtube-dl-exec/bin/**/*'],
+    },
+  },
 };
 
 export default nextConfig;
